f76fdc900d134f3c5fe0efdeecfcdd801cb05630
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-02-03 11:14:42 -0700

    commit tree b61afea67c695481145c8772122c454b298608de

--- index.js
+++ index.js
@@ -9,5 +9,17 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(timeString) {
+  let timeInt = parseInt(timeString)
+  if (timeInt <= 12) {
+    return "Good Morning"
+  } else if (12 < timeInt <= 17 ) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string){
+  document.getElementById("greeting").innerText= string
+}
